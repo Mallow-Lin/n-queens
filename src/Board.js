ef5cd9e9fd1e459c2a0bdd7af3c82b1d48a95e72
Alvin <caskgar@gmail.com>
2022-11-30 21:00:12 -0500

    Complete board through diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -83,7 +83,7 @@
       var intialValue = 0;
       for (var j = 0; j < row.length; j++) {
         if (row[j] !== 0) {
-          intialValue += row[j];
+          intialValue ++;
         }
         if (intialValue > 1) {
           return true;
@@ -115,7 +115,7 @@
       for (var i = 0; i < size; i++) {
         var row = this.get(i);
         if (row[colIndex] !== 0) {
-          intialValue += row[colIndex];
+          intialValue ++;
         }
         if (intialValue > 1) {
           return true;
@@ -141,13 +141,61 @@
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) { // -1
+
+      var initialValue = 0; // value we add in the end
+      var size = this.get('n'); // gets us SIZE
+      var columnIndex;
+      var rowIndex;
+      // used to identify our (column, rowIndex) ie: (X,Y)
+      if (majorDiagonalColumnIndexAtFirstRow >= 0) {
+        columnIndex = majorDiagonalColumnIndexAtFirstRow;
+        rowIndex = 0
+      } else {
+        columnIndex = 0;
+        rowIndex = Math.abs(majorDiagonalColumnIndexAtFirstRow);
+      }
+
+      while (columnIndex < size && rowIndex < size) {
+        var row = this.get(rowIndex);
+        if(row[columnIndex] === 1) {
+          initialValue++;
+        }
+        if (initialValue > 1) {
+          return true;
+        }
+        columnIndex++;
+        rowIndex++;
+      }
+      return false;
+
+      //APPROACH RECURSIVELY
+      // var checkNextIter = function(board, columnInput, rowInput) {
+
+      //   while (columnInput < size || rowInput < size) {
+      //   // //  var row = board[rowInput]
+      //   //   // if(row[ColumnInput] === 1) {
+      //   //   //   initialvalue++;
+      //   //   // }
+      //   //   // if (initialvalue > 1) {
+      //   //   //   return true;
+      //   //   // }
+      //       checkNextIter(board, columnInput++; rowInput++);
+      //     }
+      //     return false;
+      //   };
+      // return checkNextIter(board, columnIndex, rowIndex);
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+      for (var i = (-size+1); i < size; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -157,12 +205,42 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var initialValue = 0; // value we add in the end
+      var size = this.get('n'); // gets us SIZE
+      var columnIndex;
+      var rowIndex;
+      // used to identify our (column, rowIndex) ie: (X,Y)
+      if (minorDiagonalColumnIndexAtFirstRow <= size-1) {
+        columnIndex = minorDiagonalColumnIndexAtFirstRow;
+        rowIndex = 0
+      } else {
+        columnIndex = size - 1;
+        rowIndex = minorDiagonalColumnIndexAtFirstRow - (size - 1);
+      }
+
+      while (columnIndex > -1 && rowIndex < size) {
+        var row = this.get(rowIndex);
+        if(row[columnIndex] === 1) {
+          initialValue++;
+        }
+        if (initialValue > 1) {
+          return true;
+        }
+        columnIndex--;
+        rowIndex++;
+      }
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+      for (var i = 0; i < 2*size - 1; i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) { // checks if has conflict
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
